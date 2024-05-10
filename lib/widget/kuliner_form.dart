15a7d701a1248228bd6fa092be6e9d4bdb773871
ACER <ACER@DESKTOP-QH5VR0V>
2024-05-10 16:51:20 +0700

    add content form

--- lib/widget/kuliner_form.dart
+++ lib/widget/kuliner_form.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:exercise2/controller/kuliner_controller.dart';
 import 'package:exercise2/model/kuliner.dart';
+import 'package:exercise2/screen/homeview.dart';
 import 'package:exercise2/screen/map_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -23,6 +24,8 @@ class _FormKulinerState extends State<FormKuliner> {
   final _imagePicker = ImagePicker();
   String? _alamat;
 
+  get _id => null;
+
   Future<void> getImage() async {
     final XFile? pickedFile =
         await _imagePicker.pickImage(source: ImageSource.gallery);
@@ -30,123 +33,140 @@ class _FormKulinerState extends State<FormKuliner> {
     setState(() {
       if (pickedFile != null) {
         _image = File(pickedFile.path);
+      } else {
+        print('Tidak ada gambar');
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Form(
-          child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: "Nama", hintText: "Masukkan Nama Tempat Kuliner"),
-              controller: _namaController,
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: "Instagram", hintText: "Masukkan Akun Instagram Tempat Kuliner"),
-              controller: _instagramController,
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.all(10),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Form Resto Jogja'),
+      ),
+      body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Alamat"),
-                _alamat == null
-                    ? SizedBox(
-                        width: double.infinity,
-                        child: Text('Alamat Kosong'),
-                      )
-                    : Text('$_alamat'),
-                _alamat == null
-                    ? TextButton(
-                        onPressed: () async {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  MapScreen(onLocationSeleced: (selectedAddress) {
-                                setState(() {
-                                  _alamat = selectedAddress;
-                                });
-                              }),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Nama",
+                        hintText: "Masukkan Nama Tempat Kuliner"),
+                    controller: _namaController,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Instagram",
+                        hintText: "Masukkan Akun Instagram Tempat Kuliner"),
+                    controller: _instagramController,
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Alamat"),
+                      _alamat == null
+                          ? SizedBox(
+                              width: double.infinity,
+                              child: Text('Alamat Kosong'),
+                            )
+                          : Text('$_alamat'),
+                      _alamat == null
+                          ? TextButton(
+                              onPressed: () async {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => MapScreen(
+                                        onLocationSelected: (selectedAddress) {
+                                      setState(() {
+                                        _alamat = selectedAddress;
+                                      });
+                                    }),
+                                  ),
+                                );
+                              },
+                              child: Text('Pilih Alamat'),
+                            )
+                          : TextButton(
+                              onPressed: () async {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => MapScreen(
+                                        onLocationSelected: (selectedAddress) {
+                                      setState(() {
+                                        _alamat = selectedAddress;
+                                      });
+                                    }),
+                                  ),
+                                );
+                                setState(() {});
+                              },
+                              child: Text('Ubah Alamat'),
+                            )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Nomer Telepon", hintText: "Masukkan nomer"),
+                    controller: _noTeleponController,
+                  ),
+                ),
+                _image == null ? Text("Tidak ada gambar") : Image.file(_image!),
+                ElevatedButton(
+                  onPressed: getImage,
+                  child: Text("Pilih gambar"),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        var result = await KulinerController().addWisata(
+                            Kuliner(
+                              id: _id,
+                                nama: _namaController.text,
+                                instagram: _instagramController.text,
+                                alamat: _alamat ?? '',
+                                telepon: _noTeleponController.text,
+                                foto: _image!.path),
+                            _image);
+
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              result['message'],
                             ),
-                          );
-                        },
-                        child: Text('Pilih Alamat'),
-                      )
-                    : TextButton(
-                        onPressed: () async {
-                          Navigator.push(
+                          ),
+                        );
+
+                        Navigator.pushAndRemoveUntil(
                             context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  MapScreen(onLocationSeleced: (selectedAddress) {
-                                setState(() {
-                                  _alamat = selectedAddress;
-                                });
-                              }),
-                            ),
-                          );
-                          setState(() {});
-                        },
-                        child: Text('Ubah Alamat'),
-                      )
+                            MaterialPageRoute(builder: (context) => HomeView()),
+                            (route) => false);
+                      }
+                    },
+                    child: Text("Submit"),
+                  ),
+                )
               ],
             ),
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: "Nomer Telepon", hintText: "Masukkan nomer"),
-              controller: _noTeleponController,
-            ),
-          ),
-          _image != null ? Image.file(_image!) : Text("Tidak ada gambar"),
-          ElevatedButton(
-            onPressed: getImage,
-            child: Text("Pilih gambar"),
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            child: ElevatedButton(
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  _formKey.currentState!.save();
-                  var result = await KulinerController().addWisata(
-                      Kuliner(
-                          nama: _namaController.text,
-                          instagram: _instagramController.text,
-                          alamat: _alamat ?? '',
-                          telepon: _noTeleponController.text,
-                          foto: _image!.path),
-                      _image);
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                        result['message'],
-                      ),
-                    ),
-                  );
-                }
-              },
-              child: Text("Submit"),
-            ),
-          )
-        ],
-      )),
+          )),
     );
   }
 }
