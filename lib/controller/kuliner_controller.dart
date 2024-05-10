28e391b9437b9cae38c4b5f0d680281ffcb03372
ACER <ACER@DESKTOP-QH5VR0V>
2024-05-10 17:47:59 +0700

    add edit and delete controller

--- lib/controller/kuliner_controller.dart
+++ lib/controller/kuliner_controller.dart
@@ -3,25 +3,20 @@ import 'dart:io';
 
 import 'package:exercise2/model/kuliner.dart';
 import 'package:exercise2/service/kuliner_service.dart';
-import 'package:mysql1/mysql1.dart';
+import 'package:http/http.dart';
 
 class KulinerController {
-  final kulinerService = KulinerService();
-
-  final String _host = 'localhost';
-  final int _port = 3306;
-  final String _user = 'root';
-  final String _password = 'Dubaddu05_';
-  final String _databaseName = 'kuliner';
+  final KulinerService _service = KulinerService();
 
   Future<Map<String, dynamic>> addWisata(Kuliner wisata, File? file) async {
     Map<String, String> data = {
       'nama': wisata.nama,
       'alamat': wisata.alamat,
+      'instagram': wisata.instagram,
       'telepon': wisata.telepon
     };
     try {
-      var response = await kulinerService.addWisata(data, file);
+      var response = await _service.addResto(data, file);
 
       if (response.statusCode == 201) {
         return {
@@ -54,39 +49,89 @@ class KulinerController {
   }
 
   Future<List<Kuliner>> getResto() async {
-    final connectionSettings = ConnectionSettings(
-      host: _host,
-      port: _port,
-      user: _user,
-      db: _databaseName,
-      password: _password,
-    );
-
-    final MySqlConnection connection =
-        await MySqlConnection.connect(connectionSettings);
-
     try {
-      final results = await connection.query('SELECT * FROM KULINER');
-
-      final kulinerList = results
-          .map((row) => Kuliner(
-                nama: row[0],
-                instagram: row[1],
-                alamat: row[2],
-                telepon: row[3],
-                foto: row[4],
-              ))
-          .toList();
-
-      List<dynamic> restoData = await kulinerService.fetchResto();
+      List<dynamic> restoData = await _service.fetchResto();
       List<Kuliner> resto =
           restoData.map((json) => Kuliner.fromMap(json)).toList();
       return resto;
     } catch (e) {
-      print(e);
+      print('Response: $Response');
+      print('Exception: $e');
       throw Exception('Failed to get resto');
-    } finally {
-      await connection.close();
+    }
+  }
+
+  Future<Map<String, dynamic>> editResto(
+      Kuliner kuliner, File? file, String id) async {
+    Map<String, String> data = {
+      'nama': kuliner.nama,
+      'instagram': kuliner.instagram,
+      'alamat': kuliner.alamat,
+      'telepon': kuliner.telepon,
+    };
+
+    try {
+      var response = await _service.editResto(data, file, id);
+
+      if (response.statusCode == 200) {
+        return {
+          'success': true,
+          'message': 'Data berhasil diubah',
+        };
+      } else {
+        if (response.headers['content-type']!.contains('application/json')) {
+          var decodedJson = jsonDecode(response.body);
+          return {
+            'success': false,
+            'message': decodedJson['message'] ?? 'Terjadi kesalahan',
+          };
+        }
+
+        var decodedJson = jsonDecode(response.body);
+        return {
+          'success': false,
+          'message':
+              decodedJson['message'] ?? 'Terjadi Kesalahan saat mengubah data',
+        };
+      }
+    } catch (e) {
+      return {
+        'success': false,
+        'message': 'Terjadi kesalahan: $e',
+      };
+    }
+  }
+
+  Future<Map<String, dynamic>> deleteResto(String id) async {
+    try {
+      var response = await _service.deleteResto(id);
+
+      if (response.statusCode == 200) {
+        return {
+          'success': true,
+          'message': 'Data berhasil dihapus',
+        };
+      } else {
+        if (response.headers['content-type']!.contains('application/json')) {
+          var decodedJson = jsonDecode(response.body);
+          return {
+            'success': false,
+            'message': decodedJson['message'] ?? 'Terjadi Kesalahan',
+          };
+        }
+
+        var decodedJson = jsonDecode(response.body);
+        return {
+          'success': false,
+          'message':
+              decodedJson['message'] ?? 'Terjadi Kesalahan saat menghapus data',
+        };
+      }
+    } catch (e) {
+      return {
+        'success': false,
+        'message': 'Terjadi kesalahan: $e',
+      };
     }
   }
 }
