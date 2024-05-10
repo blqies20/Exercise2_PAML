a0f1a9ee7c7252108771b713db5061af47df329e
ACER <ACER@DESKTOP-QH5VR0V>
2024-05-10 17:46:04 +0700

    add get database

--- lib/service/kuliner_service.dart
+++ lib/service/kuliner_service.dart
@@ -4,14 +4,14 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class KulinerService {
-  final String baseUrl = 'http://paml.tatiumy.com/api/';
-  final String endpoint = 'resto';
+  final String baseUrl = 'http://10.0.2.2/kulinerApi/';
+  final String endpoint = 'api.php';
 
   Uri getUri(String path) {
     return Uri.parse("$baseUrl$path");
   }
 
-  Future<http.Response> addWisata(Map<String, String> data, File? file) async {
+  Future<http.Response> addResto(Map<String, String> data, File? file) async {
     var request = http.MultipartRequest(
       'POST',
       getUri(endpoint),
@@ -36,10 +36,30 @@ class KulinerService {
           "Accept": "application/json",
         });
     if (response.statusCode == 200) {
-      final Map<String, dynamic> decodedResponse = json.decode(response.body);
-      return decodedResponse['resto'];
+      final List<dynamic> decodedResponse = json.decode(response.body);
+      return decodedResponse;
     } else {
       throw Exception('Failed to load resto: ${response.reasonPhrase}');
     }
+  }
+
+  Future<http.Response> editResto(
+      Map<String, String> data, File? file, String id) async {
+    var request = http.MultipartRequest(
+      'PUT',
+      getUri('$endpoint/$id'),
+    )
+      ..fields.addAll(data)
+      ..headers['Content-Type'] = 'multipart/form-data';
+
+    if (file != null) {
+      request.files.add(await http.MultipartFile.fromPath('gambar', file.path));
+    }
+
+    return await http.Response.fromStream(await request.send());
+  }
+
+  Future<http.Response> deleteResto(String id) async {
+    return await http.delete(getUri('$endpoint/$id'));
   }
 }
